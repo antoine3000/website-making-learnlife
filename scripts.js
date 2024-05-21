270d20a26466beb904d165aafd01e96b30e89e8f
antoine3000 <antoine.stuff@pm.com>
2024-05-21 15:57:07 +0200

    content and img

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // Function to shuffle an array
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+//   }
 
   // Function to randomize the order of articles
   function randomizeOrder() {
